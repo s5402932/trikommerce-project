1831fbf93a254974794efe993a35921de4d8ce5a
Seb Evans <s5402932@bournemouth.ac.uk>
2024-11-17 12:02:37 +0000

    Fix "Error: Totals do not match"

--- restock.py
+++ restock.py
@@ -1,4 +1,9 @@
 def restock_inventory(available_items, inventory_records, current_day):
+    # This first if statement fixes "Error: Totals do not match".
+    # Without it, the code works fine, but on day 0, restocked_units will be 0 instead of 2000.
+    if current_day == 0: 
+        available_items = 0
+
     if current_day % 7 == 0:
         max_available_items = 2000
         sold_units = 0
