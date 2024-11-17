5e945fb4f2c12b4fa5fd80e613e85c9363626755
Seb Evans <s5402932@bournemouth.ac.uk>
2024-11-17 11:08:48 +0000

    Calculate inventory_records in restock.py

--- restock.py
+++ restock.py
@@ -1,21 +1,9 @@
 def restock_inventory(available_items, inventory_records, current_day):
-    '''
-***********COMPLETE THIS FUNCTION***********
-This function is responsible for updating the stock/restock for a given day.
----------------
-Function Input:
----------------
-available_items: (integer) Available Tshirts from the previous day.
-inventory_records: (List) A list of inventory records until the previous day. Each row contains (day, sales, restocked items, available items)
-current_day: (integer) Day number which you want to add as the current day. 
-
-----------------
-Function Output:
-----------------
-available_items:(integer) This function returns this integer which updates the available items at the current day.
-
-
-The function will also update the inventory_records (For restocking) for a  given current day. It will also return "available_items".
-    '''
-
+    if current_day % 7 == 0:
+        max_available_items = 2000
+        sold_units = 0
+        restocked_units = max_available_items - available_items
+        available_items += restocked_units
+        inventory_records.append((current_day, sold_units, restocked_units, available_items))
+    
     return available_items
