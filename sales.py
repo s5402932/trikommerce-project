d565e11b2d8fd9bd446369b366cd80057acfec2e
Seb Evans <s5402932@bournemouth.ac.uk>
2024-11-17 11:29:24 +0000

    Calculate inventory_records in sales.py

--- sales.py
+++ sales.py
@@ -1,23 +1,10 @@
 import random
 
 def daily_sales(available_items, inventory_records, current_day):
-    '''
-***********COMPLETE THIS FUNCTION***********
-This function is responsible for updating the sales for a given day.
----------------
-Function Input:
----------------
-available_items: (integer) Available Tshirts from the previous day.
-inventory_records: (List) A list of inventory records until the previous day. Each row contains (day, sales, restocked items, available items)
-current_day: (integer) Day number which you want to add as the current day. 
+    if current_day % 7 != 0:
+        sold_units = random.randint(1, 200)
+        restocked_units = 0
+        available_items -= sold_units
+        inventory_records.append((current_day, sold_units, restocked_units, available_items))
 
-----------------
-Function Output:
-----------------
-available_items:(integer) This function returns this integer which updates the available items at the current day.
-
-The function will also update the inventory_records (For restocking) for a  given current day. 
-
-    '''
-    
     return available_items
